92817bcdbfdc3b1d19014b3824070eb90f9d8a03
SophiaBelkin <sophia.lu@Sophias-Mini.socal.rr.com>
2021-07-19 21:02:10 -0700

    Added initial file

---
+++ OnTheMap/OnTheMap/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  OnTheMap
+//
+//  Created by Sophia Lu on 7/18/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
