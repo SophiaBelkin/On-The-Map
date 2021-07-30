4504d19c7fd6cbcae9130102f3c4d3d035c6e847
SophiaBelkin <sophia.lu@belkin.com>
2021-07-30 02:04:16 -0700

    Added get locations api changes

---
+++ OnTheMap/OnTheMap/Util/Global.swift
@@ -0,0 +1,13 @@
+//
+//  System.swift
+//  OnTheMap
+//
+//  Created by Sophia Lu on 7/30/21.
+//
+
+import Foundation
+
+
+struct Global {
+    static var studentsInfo: [StudentInfo] = []
+}
---
+++ OnTheMap/OnTheMap/Util/UserLocationRequest.swift
@@ -0,0 +1,8 @@
+//
+//  UserLocationRequest.swift
+//  OnTheMap
+//
+//  Created by Sophia Lu on 7/30/21.
+//
+
+import Foundation
